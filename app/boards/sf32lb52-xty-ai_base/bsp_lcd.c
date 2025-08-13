ebc5c99da3721722ed2c0df8824ecdc0a08331f4
HalfSweet <haoranjiang@sifli.com>
2025-08-13 18:22:17 +0800

    fix: 修复THT板若干问题
    - 功耗
    - 按键

--- app/boards/sf32lb52-xty-ai_base/bsp_lcd.c
+++ app/boards/sf32lb52-xty-ai_base/bsp_lcd.c
@@ -18,14 +18,32 @@ void BSP_LCD_PowerDown(void)
 {
     // TODO: LCD power down
     BSP_GPIO_Set(LCD_BL_PIN, 0, 1);
+    BSP_GPIO_Set(32, 1, 1);
+    BSP_GPIO_Set(LCD_RESET_PIN, 0, 1);
+
+    BSP_GPIO_Set(21, 0, 1);
+
+    HAL_PIN_Set(PAD_PA00, GPIO_A0, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA01, GPIO_A1, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA02, GPIO_A2, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA03, GPIO_A3, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA04, GPIO_A4, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA05, GPIO_A5, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA06, GPIO_A6, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA07, GPIO_A7, PIN_PULLDOWN, 1);
+    HAL_PIN_Set(PAD_PA08, GPIO_A8, PIN_PULLDOWN, 1);
 }
 
 void BSP_LCD_PowerUp(void)
 {
     // TODO: LCD power up
     BSP_GPIO_Set(LCD_BL_PIN, 1, 1);
+    BSP_GPIO_Set(LCD_RESET_PIN, 1, 1);
+    
     HAL_Delay_us(500);      // lcd power on finish ,need 500us
     BSP_PIN_LCD();
+
+    HAL_PIN_Set(PAD_PA00, GPIO_A0, PIN_NOPULL, 1);
 }
 
 #endif
--- app/boards/sf32lb52-xty-ai_base/bsp_power.c
+++ app/boards/sf32lb52-xty-ai_base/bsp_power.c
@@ -27,17 +27,43 @@ __WEAK void BSP_PowerUpCustom(bool is_deep_sleep)
     HAL_PMU_ConfigPeriLdo(PMUC_PERI_LDO_EN_VDD33_LDO3_Pos, 1, 1);
 }
 
-
+extern void *rt_flash_get_handle_by_addr(uint32_t addr);
 void BSP_Power_Up(bool is_deep_sleep)
 {
     BSP_PowerUpCustom(is_deep_sleep);
+#ifdef SOC_BF0_HCPU
+    if (!is_deep_sleep)
+    {
+#ifdef BSP_USING_PSRAM1
+        bsp_psram_exit_low_power("psram1");
+#endif /* BSP_USING_PSRAM1 */
+#ifdef BSP_USING_NOR_FLASH2
+        FLASH_HandleTypeDef *flash_handle;
+        flash_handle = (FLASH_HandleTypeDef *)rt_flash_get_handle_by_addr(MPI2_MEM_BASE);
+        HAL_FLASH_RELEASE_DPD(flash_handle);
+        HAL_Delay_us(80);
+#endif
+    }
+#endif  /* SOC_BF0_HCPU */
 }
-
-
 
 void BSP_IO_Power_Down(int coreid, bool is_deep_sleep)
 {
     BSP_PowerDownCustom(coreid, is_deep_sleep);
+#ifdef SOC_BF0_HCPU
+    if (coreid == CORE_ID_HCPU)
+    {
+#ifdef BSP_USING_PSRAM1
+        bsp_psram_enter_low_power("psram1");
+#endif /* BSP_USING_PSRAM1 */
+#ifdef BSP_USING_NOR_FLASH2
+        FLASH_HandleTypeDef *flash_handle;
+        flash_handle = (FLASH_HandleTypeDef *)rt_flash_get_handle_by_addr(MPI2_MEM_BASE);
+        HAL_FLASH_DEEP_PWRDOWN(flash_handle);
+        HAL_Delay_us(3);
+#endif
+    }
+#endif  /* SOC_BF0_HCPU */
 }
 
 void BSP_SDIO_Power_Up(void)
