fa6808d9c19a49d08feb785e2499f7dac6fc38ed
zhenpeng <100991395+yunlonguu@users.noreply.github.com>
2025-08-22 14:57:41 +0800

    Mbedtls (#98)
    * 添加待机界面文字提示，修改小智连接方式
    
    * 将mbedtls的heap设置，打开MBEDTLS_PLATFORM_MEMORY,sdk更新节点-》2872e

--- app/src/opus_heap.c
+++ app/src/opus_heap.c
@@ -17,13 +17,10 @@ L2_RET_BSS_SECT_END
 
 static struct rt_memheap opus_memheap;
 
-int opus_heap_init(void)
-{
-    rt_memheap_init(&opus_memheap, "opus_memheap", (void *)opus_heap_pool,
-                    sizeof(opus_heap_pool));
-    return 0;
-}
-INIT_BOARD_EXPORT(opus_heap_init);
+//TODO: 
+int mbedtls_platform_set_calloc_free( void * (*calloc_func)( size_t, size_t ),
+                              void (*free_func)( void * ) );
+
 
 void *opus_heap_malloc(uint32_t size)
 {
@@ -34,3 +31,19 @@ void opus_heap_free(void *p)
 {
     rt_memheap_free(p);
 }
+
+void *opus_heap_calloc(size_t count, size_t size)
+{
+    return rt_memheap_calloc(&opus_memheap, count, size);
+}
+
+int opus_heap_init(void)
+{
+    rt_memheap_init(&opus_memheap, "opus_memheap", (void *)opus_heap_pool,
+                    sizeof(opus_heap_pool));
+
+
+    mbedtls_platform_set_calloc_free(opus_heap_calloc, opus_heap_free);
+    return 0;
+}
+INIT_BOARD_EXPORT(opus_heap_init);
